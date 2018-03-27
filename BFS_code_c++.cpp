7806417cc67d6209423eb426b95493facb3b66b7
AndreattoB <byandreatto@hotmail.com>
2018-03-27 20:02:40 -0300

    Signed-off-by: AndreattoB <byandreatto@hotmail.com>

---
+++ BFS_code_c++.cpp
@@ -0,0 +1,18 @@
+// Algorithm BFS
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector< vector <int> > grafo; // Criando a lista de  adjacência 
+
+vector<int> BFS (int inicio){
+	
+	
+}
+
+
+int main(){
+	
+	return 0;
+}
